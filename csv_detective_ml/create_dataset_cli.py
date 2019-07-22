1d3a1936f4c3fc521a454eb8dc68117098b5124e
Pavel Soriano <pavel.soriano@data.gouv.fr>
2019-07-22 10:15:33 +0200

    New script to create a test dataset from full_dgf

---
+++ csv_detective_ml/create_dataset_cli.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import re
+import numpy as np
+import json
+
+
+def run():
+    csv_data = json.load("/home/pavel/etalab/code/csv_detective_api/csv_data_full.json")
+
+
+    pass
+
+
+if __name__ == '__main__':
+    pass
