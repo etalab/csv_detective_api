e7b0ef4f58738cb92a1ffc525d091f1422c14146
Pavel <pavel.soriano@data.gouv.fr>
2019-08-27 22:59:45 +0200

    reworked the about

--- ui/src/App.js
+++ ui/src/App.js
@@ -337,13 +337,13 @@ class App extends Component {
             <Col><h5>What?</h5>
 
             <a href="https://github.com/etalab/csv_detective">CSV Detective</a> is a tool that gives you information about a CSV, such as its encoding and separator, as well as the type of columns contained inside: whether there are columns containing a <i>SIRET</i> or a <i>SIREN</i> number, a postal code, a department or a commune name, a geographic position, etc. 
-            <p>This UI builds on CSV Detective. We improved it, APIfied it, and through this interface, allow a friendlier use. Also a machine learning model to detect types was added (which is work in progress).
+            <p>This UI builds on CSV Detective. We improved it, APIfied it, and through this interface, you can get a glimpse of how it works. Also, we added a machine learning model as an alternative  (this is work in progress).
             </p>
           </Col>
           </Row>
             <Row>
             <Col><h5>Why?</h5>
-            This tool was developed  with <a href="https://www.data.gouv.fr" target="_blank" rel="noopener noreferrer">data.gouv.fr</a> (DGF) in mind. Being a repository of open datasets is one of the main tasks of DGF. In that sense, knowing what is inside the large collection of CSVs it contains can be useful for several tasks:
+            This tool was developed  with <a href="https://www.data.gouv.fr" target="_blank" rel="noopener noreferrer">data.gouv.fr</a> (DGF) in mind. One of the main tasks of DGF is being a repository of open datasets. These datasets are often contined within CSV files. Knowing what is inside this large collection of CSVs can be useful for several tasks:
             <ul>
               <li><b>Enrich</b> the results of the <b>search engine</b> with the contents of the CSVs.</li>
               <li><b>Link datasets together</b> according to their values.</li>
@@ -354,15 +354,16 @@ class App extends Component {
             </Row> 
             <Row>
             <Col><h5>How?</h5>
-            CSV Detective has two strategies to detect a column type:
+            The UI has two modes of functioning: you can enter a DGF resource ID and clicking the button or you can load a CSV file by clicking in the corresponding area. The process in both cases will start automatically.
+            Behind the scenes, CSV Detective has two strategies to detect a column type:
             <ol>
               <li><b>Rules + References</b>: using regular expressions and also comparing the values with reference data (e.g., if the value <i>69007</i> appears in a list of postal codes, then it is a postal code.</li>
               <li><b>Supervised Learning (In progress)</b>: manually tagging columnt types and then determining simple features coupled to the content of the cells themselves to train classification algorithms.</li>
             </ol>
             </Col>
             </Row>
-            <Row><Col><h5>Where?</h5>
-            The code lives <a href="https://github.com/psorianom/csv_detective_api" target="_blank" rel="noopener noreferrer">here</a>.
+            <Row><Col><h5>Who?</h5>
+            Etalab's Data Science, Datagouv, and Open Data teams are working on this. The code lives <a href="https://github.com/psorianom/csv_detective_api" target="_blank" rel="noopener noreferrer">here</a> for now.
             
             </Col></Row>
             </div>
